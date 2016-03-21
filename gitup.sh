d2631f263ec8ce3fbefa9196bc2709807fd030be
nextkara <nextkara@hotmail.com>
2016-03-21 15:31:41 +0800

    change

--- gitup.sh
+++ gitup.sh
@@ -1,3 +1,12 @@
 #!/bin/bash
 #this is new line
 #other line
+echo "What's change in new file"
+echo "Please live a message for update:"
+git status -s
+echo "Please wait 3 seconds..."
+sleep 3
+git add .
+git commit -m "$1"
+git push 
+
