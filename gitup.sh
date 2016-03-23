730e36ab0cd554af1fd928f13cb7478dfd78b702
nextkara <nextkara@hotmail.com>
2016-03-23 20:31:35 +0800

    fix some bugs

--- gitup.sh
+++ gitup.sh
@@ -1,12 +1,17 @@
 #!/bin/bash
 #this is new line
 #other line
+if [ $1 == null ];then
+  CHANGE = "Something change we don't know"
+else 
+   CHANGE = $1
+fi
 echo "What's change in new file"
 echo "Please live a message for update:"
 git status -s
 echo "Please wait 3 seconds..."
 sleep 3
 git add .
-git commit -m "$1"
+git commit -m "$CHANGE"
 git push 
 
---
+++ global.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python
+#python strip
+Tom = 10
+def Test():
+  Tom = 0
+  print(Tom)
+print(Tom)
+Test()
