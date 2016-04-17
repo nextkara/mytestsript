d3859994d5af4dee49206d0e0672a640867a3d94
nextkara <nextkara@hotmail.com>
2016-04-17 14:05:13 +0800

    test some OOP python stripts

---
+++ person.py
@@ -0,0 +1,40 @@
+#!/usr/bin/python
+class Person:
+    def __init__(self, name, job = 0, pay = 0):
+        'version 1.0'
+        self.name = name
+        self.job = job
+        self.pay = pay
+
+class Manager(Person):
+    'version 1.3'
+    def giveRaise(self, percent, bonus=.10):
+        self.pay = int(self.pay * (1 + percent + bonus))
+    
+    def lastName(self):
+        'version 1.1'
+        return self.name.split()[-1]
+    
+    def giveRaise(self, percent):
+        'version 1.1'
+        self.pay = int(self.pay * (1 + percent))
+        
+    def __str__(self):
+        'version 1.2'
+        return '[Person : %s %s ]'.%(self.name, self.pay)
+    
+    if __init__ == '__main__':
+        bob = Person('Bob Smith')
+        sue = Person('Sue Jones',job = 'Tech', pay = 1000)
+        print(bob.name, bob.pay)
+        'version 1.1'
+        print(bob.lastName(), sue.lastName())
+        sue.giveRaise(.10)
+        print(sue.pay)
+        '''
+        version 1.0
+        print(sue.name, sue.pay)
+        sue.pay *= 1.10
+        print(sue.pay)
+        '''
+        
--- ssl.sh
+++ ssl.sh
@@ -3,3 +3,7 @@
 #the mod of the key must be only root can read and write
 openssl genrsa -out sever.key 1024
 (umask 077;openssl rsa -in sever.key -pubout -out pub.key)
+openssl req -new x509 -key sever.key -out sever.crt
+cat << EOF
+
+penssl x509 -text -in sever.crt
