05f0b9df1f6da6cb5aa25d3be71bf55d587b3144
nextkara <nextkara@hotmail.com>
2016-06-05 20:09:53 +0800

    something change in linux

--- fu.sh
+++ fu.sh
@@ -1,9 +1,28 @@
 #!/bin/bash
 #
-i=1
-while i=1;do
-  echo '----------------------------------------------------'
-  fortune
-  echo '----------------------------------------------------'
-  sleep 2
-done
+
+pip install pylint
+
+
+pip install pep8
+
+
+pip install flake8
+
+
+pip install pydocstyle
+
+
+pip install prospector
+
+
+pip install pep8
+pip install --upgrade autopep8
+
+
+pip install yapf
+
+
+pip install nose
+
+
